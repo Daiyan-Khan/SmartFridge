73f05d465c5d387af16bd7aa826acebc02669c1b
Daiyan-Khan <114041913+Daiyan-Khan@users.noreply.github.com>
2023-09-19 20:20:51 +1000

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -14,16 +14,15 @@ while True:
     
     # if there is a bounding box, draw one, along with the data
     if(bbox is not None):
-        for i in range(len(bbox)):
-            cv2.line(img, tuple(bbox[i][0]), tuple(bbox[(i+1) % len(bbox)][0]), color=(255,
-                     0, 255), thickness=2)
+        for i in range(len(bbox[0])):
+            cv2.line(img, tuple(bbox[0][i]), tuple(bbox[0][(i+1) % 4]), color=(255, 0, 255), thickness=2)
         cv2.putText(img, data, (int(bbox[0][0][0]), int(bbox[0][0][1]) - 10), cv2.FONT_HERSHEY_SIMPLEX,
                     0.5, (0, 255, 0), 2)
         if data:
-            print("data found: ", data)
+            print("data found:", data)
     # display the image preview
     cv2.imshow("code detector", img)
-    if(cv2.waitKey(1) == ord("q")):
+    if cv2.waitKey(1) == ord("q"):
         break
 # free camera object and exit
 cap.release()
