126ee4528d061b99bede8267f303da3f5b0cdcc9
Daiyan-Khan <114041913+Daiyan-Khan@users.noreply.github.com>
2023-09-19 20:24:10 +1000

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,29 +1,25 @@
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
 import cv2
-
-# set up camera object
-cap = cv2.VideoCapture(0)
-
-# QR code detection object
-detector = cv2.QRCodeDetector()
-
-while True:
-    # get the image
-    _, img = cap.read()
-    # get bounding box coords and data
-    data, bbox, _ = detector.detectAndDecode(img)
-    
-    # if there is a bounding box, draw one, along with the data
-    if(bbox is not None):
-        for i in range(len(bbox[0])):
-            cv2.line(img, tuple(bbox[0][i]), tuple(bbox[0][(i+1) % 4]), color=(255, 0, 255), thickness=2)
-        cv2.putText(img, data, (int(bbox[0][0][0]), int(bbox[0][0][1]) - 10), cv2.FONT_HERSHEY_SIMPLEX,
-                    0.5, (0, 255, 0), 2)
-        if data:
-            print("data found:", data)
-    # display the image preview
-    cv2.imshow("code detector", img)
-    if cv2.waitKey(1) == ord("q"):
-        break
-# free camera object and exit
-cap.release()
-cv2.destroyAllWindows()
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+# allow the camera to warmup
+time.sleep(0.1)
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	# grab the raw NumPy array representing the image, then initialize the timestamp
+	# and occupied/unoccupied text
+	image = frame.array
+	# show the frame
+	cv2.imshow("Frame", image)
+	key = cv2.waitKey(1) & 0xFF
+	# clear the stream in preparation for the next frame
+	rawCapture.truncate(0)
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
