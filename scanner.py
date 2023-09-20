a25dff25955c5092892b4cc1ca230f9ad086bb6d
Daiyan-Khan <114041913+Daiyan-Khan@users.noreply.github.com>
2023-09-20 11:20:04 +1000

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,29 +1,37 @@
-from picamera import PiCamera
+from picamera2 import Picamera2, Preview
+from PIL import Image
 from time import sleep
 import zbarlight
-
+import requests
 # Initialize the camera
-camera = PiCamera()
+camera = Picamera2()
 
 # Set camera resolution (adjust as needed)
-camera.resolution = (640, 480)
+camera.resolution = (300, 300)
 
 # Start capturing video
-camera.start_preview()
-
+camera_config = camera.create_preview_configuration()
+camera.configure(camera_config)
+camera.start_preview(Preview.QTGL)
+camera.start()
 while True:
     # Capture a frame
     sleep(1)  # Adjust the sleep duration as needed to control frame rate
-    camera.capture('frame.jpg')
+   
+   
+    camera.capture_file("frame.jpg")
+    sleep(2)
 
     # Read the captured image and decode QR codes
     with open('frame.jpg', 'rb') as image_file:
-        image_data = image_file.read()
-        codes = zbarlight.scan_codes('qrcode', image_data)
+        image_data = Image.open(image_file)
+        image_data.load()
+        codes = zbarlight.scan_codes(['qrcode'], image_data)
 
         if codes:
             for code in codes:
-                print("QR Code data:", code.decode('utf-8'))
-
+                item = code.decode('utf8')
+                print("QR Code data:", item)
+                requests.post('https://maker.ifttt.com/trigger/pic_taken/{"value1" : "item"}/with/key/dqisEg_LCCBlGCCu3DDNbPSY7F0lYojB3y-UTwBn0fX')
 # Stop capturing and close the camera
 camera.stop_preview()
